8c334feb092157bec40db0c4df66f50870eb1bfa
Alexander Gusev <sanekgusev@gmail.com>
2012-04-22 16:22:59 +0400

    Removed extra whitespace.

--- src/com/neuro_immune_detector_core/services/ReferenceVectorsCreator.java
+++ src/com/neuro_immune_detector_core/services/ReferenceVectorsCreator.java
@@ -8,11 +8,6 @@ import com.neuro_immune_detector_core.exceptions.NeuroImmuneDetectorException;
 import com.neuro_immune_detector_core.policies.DistributionPolicy;
 import com.neuro_immune_detector_core.repositories.FileRepository;
 
-
-
-
-
-
 public interface ReferenceVectorsCreator {
 	Collection<ReferenceVectorContainer> createReferenceVectors(FileRepository infectedFiles, 
 			FileRepository cleanFiles, int numberOfVectors, DistributionPolicy distributionPolicy,
